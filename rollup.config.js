7aa9cf7b851fc43dc07515fefd06b68660384bdd
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2017-05-09 15:18:16 -0500

    incorporate js-cookie

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,16 @@
-import babel from 'rollup-plugin-babel';
+import buble from 'rollup-plugin-buble'
 import fs from 'fs'
 
 const pkg = JSON.parse(fs.readFileSync('./package.json'))
 
 export default {
   entry: 'src/index.js',
-  external: ['react'],
+  external: ['react, js-cookie'],
   exports: 'named',
   globals: { react: 'React' },
   useStrict: false,
   sourceMap: true,
-  plugins: [babel({
-    exclude: 'node_modules/**'
-  })],
+  plugins:[buble()],
   targets: [
     {dest: pkg.main, format: 'cjs'},
     {dest: pkg.module, format: 'es'},
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import Cookies from 'js-cookie';
+import React from 'react';
+
 export function selectWinner(weights) {
   let i, probability = 0;
   const ar = [];
@@ -12,15 +15,53 @@ export function selectWinner(weights) {
   return i;
 }
 
-const Experiment = ({
-  onSelect,
-  variants,
-}) => {
-  const chances = variants.map(v => v.chance);
-  const index = selectWinner(chances);
-  const Variant = variants[index].component;
-  onSelect(variants[index]);
-  return Variant;
-};
+function getExperiment (props) {
+    const { onSelect, variants } = props;
+    const chances = variants.map(v => v.chance);
+    const index = selectWinner(chances);
+    const Variant = variants[index].component;
+    onSelect(variants[index]);
+    return Variant;
+}
+
+function getVariationName (variants) {
+    const chances = variants.map(v => v.chance);
+    const index = selectWinner(chances);
+    const variationName = variants[index].name;
+    return index;
+}
+
+function getExperiment (name) {
+    return Cookies.get(`experiment_${name}`);
+}
+
+function setExperiment (name, index) {
+    return Cookies.set(`experiment_${name}`, index);
+}
+
+function removeExperiment (name) {
+    return Cookies.remove(`experiment_${name}`)
+}
+
+class Experiment extends React.Component {
+    componentWillMount () {
+        const { name, variants } = this.props;
+        const exp = getExperiment(name);
+
+        if (exp) {
+            this.variation = exp;
+        } else {
+            this.variation = getVariationName(variants);
+            setExperiment(name, this.variation);
+        }
+
+        this.Component = variants[this.variation].component;
+    }
+
+    render () {
+        const Component = this.Component;
+        return Component;
+    }
+}
 
 export default Experiment
