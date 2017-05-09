18c0668ff4c3a043513e34f7c354ade97fcf8b7f
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2017-05-09 16:20:03 -0500

    commonjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,22 @@
-import buble from 'rollup-plugin-buble'
+import commonjs from 'rollup-plugin-commonjs';
+import buble from 'rollup-plugin-buble';
 import fs from 'fs'
 
 const pkg = JSON.parse(fs.readFileSync('./package.json'))
 
 export default {
   entry: 'src/index.js',
-  external: ['react, js-cookie'],
+  external: ['react', 'js-cookie'],
   exports: 'named',
-  globals: { react: 'React' },
+  globals: { react: 'React', 'js-cookie': 'Cookies' },
   useStrict: false,
   sourceMap: true,
-  plugins:[buble()],
+  plugins:[
+    commonjs({
+      sourceMap: false,
+    }),
+    buble()
+  ],
   targets: [
     {dest: pkg.main, format: 'cjs'},
     {dest: pkg.module, format: 'es'},
